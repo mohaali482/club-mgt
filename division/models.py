fb0fb4e23e452c1517c0ef09c96c765f1a8dfbe3
Mohammed Ali <mohaali482@gmail.com>
2022-05-09 19:24:43 +0300

    added a divisions model

--- division/models.py
+++ division/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Division(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=50)
+    active = models.BooleanField()
+    created_at = models.DateTimeField(auto_now_add=False)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.name)
