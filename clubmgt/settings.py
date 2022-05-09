27d1c0980cd660a24552c0a5ab46f98fc31a55d7
Mohammed Ali <mohaali482@gmail.com>
2022-05-09 12:14:43 +0300

    added the apps to the installed apps and added the templates directory

--- clubmgt/settings.py
+++ clubmgt/settings.py
@@ -41,7 +41,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'phonenumber_field'
+    'phonenumber_field',
+    'authentication',
+    'division',
+    'event',
+    'feed',
+
 ]
 
 MIDDLEWARE = [
@@ -59,7 +64,7 @@ ROOT_URLCONF = 'clubmgt.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
