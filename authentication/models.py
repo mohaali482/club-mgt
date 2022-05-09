96fab0b6470026d6df9ad80958e060d33ffe8bdc
Mohammed Ali <mohaali482@gmail.com>
2022-05-09 10:56:28 +0300

    added user additional information

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+GENDER_CHOICES = [
+    ("Male", "male"),
+    ("Female", "female"),
+]
+
+class UserAdditionalInformaion(models.Model):
+    phone_no = models.PhoneNumberField()
+    gender = models.CharField(max_length=50, choices=GENDER_CHOICES)
+    year = models.PositiveIntegerField()
+    telegram_username = models.CharField(max_length=50)
