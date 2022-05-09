1055378cf269188a044d53798448076a4b7de528
Mohammed Ali <mohaali482@gmail.com>
2022-05-09 19:49:44 +0300

    added the subdivision model

--- sub_division/models.py
+++ sub_division/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class SubDivision(models.Model):
+    division = models.ForeignKey("division.Division", on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.name)
