092ec5680a31e6a543441d790c64619f4dad2220
Mohammed Ali <mohaali482@gmail.com>
2022-05-09 12:51:42 +0300

    added a login functionality

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
+    path('login/', auth_views.LoginView.as_view(template_name="authentication/login.html"), name="login" ),
     path('signup/', views.signup, name='signup'),
     path('activate/<slug:uidb64>/<slug:token>/',
         views.activate, name='activate'),
