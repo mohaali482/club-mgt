4e30d07fadd58a3fd22a6f0a2ba81d09665252e8
Mohammed Ali <mohaali482@gmail.com>
2022-05-10 13:10:25 +0300

    included division app's urls

--- clubmgt/urls.py
+++ clubmgt/urls.py
@@ -19,4 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include("authentication.urls")),
+    path('division/', include("division.urls")),
 ]
