2e187a6c444622a9f07d1b2acc65263d110e1c9f
Mohammed Ali <mohaali482@gmail.com>
2022-05-10 12:53:46 +0300

    added a method to get the encoded id

--- division/models.py
+++ division/models.py
@@ -1,6 +1,8 @@
 from django.db import models
-
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
 # Create your models here.
+
 
 class Division(models.Model):
     name = models.CharField(max_length=50)
@@ -11,3 +13,7 @@ class Division(models.Model):
 
     def __str__(self):
         return str(self.name)
+
+    def encoded_id(self) -> str:
+        encode_id = urlsafe_base64_encode(force_bytes(self.pk))
+        return encode_id
--- sub_division/models.py
+++ sub_division/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
 # Create your models here.
 
 
@@ -13,3 +14,7 @@ class SubDivision(models.Model):
 
     def __str__(self):
         return str(self.name)
+
+    def encoded_id(self) -> str:
+        encode_id = urlsafe_base64_encode(force_bytes(self.pk))
+        return encode_id
