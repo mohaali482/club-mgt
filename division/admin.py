18068a541c292685e35295afd7a3c3fa07cbcafd
Mohammed Ali <mohaali482@gmail.com>
2022-05-12 16:14:52 +0300

    Modified the division list and added division head

--- division/admin.py
+++ division/admin.py
@@ -1,8 +1,32 @@
 from django.contrib import admin
-from .models import Division
+from .models import Division, DivisionHead, AdminDivisionQuerySet
+from .forms import DivisionHeadForm, DivisionForm
 
 # Register your models here.
 
+
 @admin.register(Division)
 class DivisionAdmin(admin.ModelAdmin):
-    pass
+    list_display = (
+        "name",
+        "active",
+    )
+    form = DivisionForm
+
+    def get_queryset(self, request):
+        qs = self.model.admin_page
+        ordering = self.get_ordering(request)
+        if ordering:
+            qs = qs.order_by(*ordering)
+        return qs
+    
+
+
+@admin.register(DivisionHead)
+class DivisionHeadAdmin(admin.ModelAdmin):
+    list_display = (
+        "user",
+        "division",
+        "active",
+    )
+    form = DivisionHeadForm
