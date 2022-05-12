e933159e024a7eb3a862a22bce69c02464db652b
Mohammed Ali <mohaali482@gmail.com>
2022-05-12 07:23:32 +0300

    Added deletion and edit url

--- division/urls.py
+++ division/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from .views import DivisionCreateView, DivisionListView, DivisionDetailView
+from .views import (DivisionCreateView, DivisionDeleteView, DivisionDetailView,
+                    DivisionListView, DivisionUpdateView)
 
 urlpatterns = [
     path('register/', DivisionCreateView.as_view(), name="division-register"),
     path('list/', DivisionListView.as_view(), name="division-list"),
     path('<slug:id>/detail/', DivisionDetailView.as_view(), name="division-detail"),
+    path('<slug:id>/delete/', DivisionDeleteView.as_view(), name="division-delete"),
+    path('<slug:id>/edit/', DivisionUpdateView.as_view(), name="division-edit"),
 ]
