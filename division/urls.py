823d43e9a61a5e0d0d410a2053c8e7ae7ab35e39
Mohammed Ali <mohaali482@gmail.com>
2022-05-13 08:30:05 +0300

    Formatted the code for better looking

--- division/urls.py
+++ division/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
 from .views import (DivisionCreateView, DivisionDeleteView, DivisionDetailView,
-                    DivisionListView, DivisionUpdateView, DivisionHeadCreateView,
-                    DivisionHeadDeleteView, DivisionHeadListView)
+                    DivisionHeadCreateView, DivisionHeadDeleteView,
+                    DivisionHeadListView, DivisionListView, DivisionUpdateView)
 
 urlpatterns = [
     path('register/', DivisionCreateView.as_view(), name="division-register"),
@@ -10,7 +10,9 @@ urlpatterns = [
     path('<slug:id>/detail/', DivisionDetailView.as_view(), name="division-detail"),
     path('<slug:id>/delete/', DivisionDeleteView.as_view(), name="division-delete"),
     path('<slug:id>/edit/', DivisionUpdateView.as_view(), name="division-edit"),
-    path('head/register/', DivisionHeadCreateView.as_view(), name="division-head-register"),
+    path('head/register/', DivisionHeadCreateView.as_view(),
+         name="division-head-register"),
     path('head/', DivisionHeadListView.as_view(), name="division-head-delete"),
-    path('head/<slug:id>/delete/', DivisionHeadDeleteView.as_view(), name="division-head-delete"),
+    path('head/<slug:id>/delete/', DivisionHeadDeleteView.as_view(),
+         name="division-head-delete"),
 ]
