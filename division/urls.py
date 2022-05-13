568e1cf81b6eefaf0715ba3d1e7371f01eaf1585
Mohammed Ali <mohaali482@gmail.com>
2022-05-13 08:09:44 +0300

    Made the urls much cleaner

--- division/urls.py
+++ division/urls.py
@@ -6,11 +6,11 @@ from .views import (DivisionCreateView, DivisionDeleteView, DivisionDetailView,
 
 urlpatterns = [
     path('register/', DivisionCreateView.as_view(), name="division-register"),
-    path('list/', DivisionListView.as_view(), name="division-list"),
+    path('', DivisionListView.as_view(), name="division-list"),
     path('<slug:id>/detail/', DivisionDetailView.as_view(), name="division-detail"),
     path('<slug:id>/delete/', DivisionDeleteView.as_view(), name="division-delete"),
     path('<slug:id>/edit/', DivisionUpdateView.as_view(), name="division-edit"),
     path('head/register/', DivisionHeadCreateView.as_view(), name="division-head-register"),
-    path('head/list/', DivisionHeadListView.as_view(), name="division-head-delete"),
+    path('head/', DivisionHeadListView.as_view(), name="division-head-delete"),
     path('head/<slug:id>/delete/', DivisionHeadDeleteView.as_view(), name="division-head-delete"),
 ]
