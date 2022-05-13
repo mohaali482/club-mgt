2256e7eb9b8430a6c7854425130b02c063f3499f
Mohammed Ali <mohaali482@gmail.com>
2022-05-13 08:30:40 +0300

    Registered the sub division model

--- sub_division/admin.py
+++ sub_division/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from .models import SubDivision
 # Register your models here.
+
+@admin.register(SubDivision)
+class SubDivisionAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "division",
+        "active"
+    )
+
+    list_filter = (
+        "division",
+        "active"
+    )
