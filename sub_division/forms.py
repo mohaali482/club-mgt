e80089c86144a825d5480f973cf38cf5877e1b60
Mohammed Ali <mohaali482@gmail.com>
2022-05-13 08:31:54 +0300

    Created a form the sub division model

---
+++ sub_division/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from .models import SubDivision
+
+
+class SubDivisionForm(forms.ModelForm):
+    
+    class Meta:
+        model = SubDivision
+        fields = ("name", "division","description")
