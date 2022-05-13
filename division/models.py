fb84911a9b869323f794619dd7d1688799413131
Mohammed Ali <mohaali482@gmail.com>
2022-05-13 07:59:33 +0300

    Added a model for storing division heads

--- division/models.py
+++ division/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.db.models import Q
 from django.utils.encoding import force_bytes
 from django.utils.http import urlsafe_base64_encode
+from django.utils.translation import gettext_lazy as _
 from clubmgt.softdeletion import regenerate_field_for_soft_deletion
 # Create your models here.
 
@@ -24,6 +25,7 @@ class DivisionQuerySet(models.QuerySet):
             Q(description__icontains=query)
         )
         return self.filter(lookups)
+
 
 class AdminDivisionQuerySet(models.QuerySet):
     pass
@@ -71,23 +73,32 @@ class Division(models.Model):
         return super().delete(self)
 
 
+class DivisionHeadQuerySet(models.QuerySet):
+    def delete(self):
+        for division_head in super().all():
+            division_head_history = DivisionHeadHistory()
+            division_head_history.user = division_head.user
+            division_head_history.division = division_head.division
+            division_head_history.started_date = division_head.created_at
+            division_head_history.ended_date = timezone.now()
+            division_head_history.save()
+
+        return super().delete()
+
+
 class DivisionHeadManager(models.Manager):
-    def all(self):
-        return self.get_queryset().filter(active=True)
+    def get_queryset(self):
+        return DivisionHeadQuerySet(self.model, using=self._db)
 
 
 class DivisionHead(models.Model):
-    user = models.ForeignKey("auth.User", on_delete=models.CASCADE)
-    division = models.ForeignKey("division.Division", on_delete=models.CASCADE)
-    deleted_at = models.DateTimeField(blank=True, null=True)
+    user = models.OneToOneField("auth.User", on_delete=models.CASCADE)
+    division = models.OneToOneField(
+        "division.Division", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
-    class Meta:
-        constraints = [
-            models.UniqueConstraint(
-                fields=['user', 'division', 'deleted_at'],
-                name="head"
-            )
-        ]
+    objects = DivisionHeadManager()
 
     def __str__(self) -> str:
         return str(self.user)
@@ -97,8 +108,25 @@ class DivisionHead(models.Model):
         return encode_id
 
     def delete(self, using=None, keep_parents=False):
-        self.deleted_at = timezone.now()
-        self.save()
-
-    def hard_delete(self):
+        division_head_history = DivisionHeadHistory()
+        division_head_history.user = self.user
+        division_head_history.division = self.division
+        division_head_history.started_date = self.created_at
+        division_head_history.ended_date = timezone.now()
+        division_head_history.save()
         return super().delete(self)
+
+
+class DivisionHeadHistory(models.Model):
+    user = models.ForeignKey("auth.User", on_delete=models.CASCADE)
+    division = models.ForeignKey("division.Division", on_delete=models.CASCADE)
+    started_date = models.DateTimeField()
+    ended_date = models.DateTimeField()
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name = _("Division Head History")
+        verbose_name_plural = _("Division Head Histories")
+
+    def __str__(self):
+        return f'{self.user} head of {self.division}'
