3bda033850def37508a8ee64f748bf4024c67c26
Mohammed Ali <mohaali482@gmail.com>
2022-05-17 09:18:19 +0300

    Included signals

--- division/apps.py
+++ division/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class DivisionConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'division'
+
+    def ready(self):
+        # pylint: disable=C0415
+        import division.signals
