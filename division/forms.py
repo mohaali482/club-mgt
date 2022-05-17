b385c6a4438bbc5467499da4a915b16982397565
Mohammed Ali <mohaali482@gmail.com>
2022-05-17 09:17:35 +0300

    Removed some code for a potential error

--- division/forms.py
+++ division/forms.py
@@ -14,10 +14,3 @@ class DivisionHeadForm(forms.ModelForm):
     class Meta:
         model = DivisionHead
         fields = ("user", "division")
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields['user'] = forms.ModelChoiceField(queryset=User.objects.filter(
-            is_active=True, divisionhead__isnull=True,))
-        self.fields['division'] = forms.ModelChoiceField(queryset=Division.objects.filter(
-            active=True, divisionhead__isnull=True))
