4b9c5e83f99e6e678eba8ed32fcee3fadbdbadb0
Mohammed Ali <mohaali482@gmail.com>
2022-05-17 10:31:01 +0300

    Added event and event images

--- event/admin.py
+++ event/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Event, EventImages
 
 # Register your models here.
+
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(EventImages)
+class EventImagesAdmin(admin.ModelAdmin):
+    pass
--- event/models.py
+++ event/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+class Event(models.Model):
+    division = models.ForeignKey("division.Division", on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+    issue_date = models.DateTimeField()
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    is_public = models.BooleanField(default=True)
+    active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=False)
+    updated_at = models.DateTimeField(auto_now=False)
+
+    def __str__(self):
+        return str(self.title)
+
+
+class EventImages(models.Model):
+    event = models.ForeignKey("event.Event", on_delete=models.CASCADE)
+    image = models.ImageField(upload_to="events/")
+
+    def __str__(self):
+        return str(self.event)
