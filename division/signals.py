2f709983053a266a196786604cb5e3015648a850
Mohammed Ali <mohaali482@gmail.com>
2022-05-17 09:19:21 +0300

    Made a signal to handle when a division head is deleted

---
+++ division/signals.py
@@ -0,0 +1,15 @@
+from django.db.models.signals import pre_delete
+from django.dispatch import receiver
+from django.utils import timezone
+
+from .models import DivisionHeadHistory, DivisionHead
+
+
+@receiver(pre_delete, sender=DivisionHead)
+def create_division_history(sender, instance, using, **kwargs):
+    DivisionHeadHistory.objects.create(
+        user=instance.user,
+        division=instance.division,
+        started_date=instance.created_at,
+        ended_date=timezone.now()
+    )
