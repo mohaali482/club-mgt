9ded3ec92b1ce9852c090959d316fa391b24534e
Mohammed Ali <mohaali482@gmail.com>
2022-05-18 22:10:00 +0300

    Imported the signal into apps

--- event/apps.py
+++ event/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class EventConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'event'
+
+    def ready(self):
+        # pylint: disable=C0415
+        import event.signals
